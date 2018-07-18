6ed7fd809b515f542e76a477129c71fab035a292
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-18 12:22:49 +0300

    Migrate readme yaml (#2)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "policy_id" {
-  value = "${aws_iam_policy.default.id}"
+  value       = "${aws_iam_policy.default.id}"
+  description = "Policy ID"
 }
 
 output "policy_name" {
-  value = "${aws_iam_policy.default.name}"
+  value       = "${aws_iam_policy.default.name}"
+  description = "Policy name"
 }
 
 output "policy_arn" {
-  value = "${aws_iam_policy.default.arn}"
+  value       = "${aws_iam_policy.default.arn}"
+  description = "Policy ARN"
 }
